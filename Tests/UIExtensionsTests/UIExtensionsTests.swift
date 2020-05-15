604d345f389fe222b066a0a1c27300cc93c306ca
Luis Reisewitz <luis.reisewitz@teufel.de>
2020-05-15 12:32:29 +0200

    Dynamic library & tests

--- Tests/UIExtensionsTests/UIExtensionsTests.swift
+++ Tests/UIExtensionsTests/UIExtensionsTests.swift
@@ -6,7 +6,7 @@ final class UIExtensionsTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(UIExtensions().text, "Hello, World!")
+        XCTAssert(true)
     }
 
     static var allTests = [
