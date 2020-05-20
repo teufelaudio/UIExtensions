ce0e354fd3909ecbc0739ab8054b351bc86ea96a
Luis Reisewitz <luis.reisewitz@teufel.de>
2020-05-20 13:54:02 +0200

    Remove Lottie Dependency

--- Package.swift
+++ Package.swift
@@ -6,23 +6,18 @@ import PackageDescription
 let package = Package(
     name: "UIExtensions",
     platforms: [
-//        .macOS(SupportedPlatform.MacOSVersion.v10_15),
         .iOS(SupportedPlatform.IOSVersion.v13),
-//        .tvOS(SupportedPlatform.TVOSVersion.v13),
-//        .watchOS(SupportedPlatform.WatchOSVersion.v6)
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "UIExtensions",
-            type: .dynamic,
             targets: ["UIExtensions"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         .package(name: "SwiftRex", url: "https://github.com/SwiftRex/SwiftRex", .branch("master")),
         .package(url: "https://github.com/teufelaudio/FoundationExtensions", .branch("master")),
-        .package(name: "Lottie", url: "https://github.com/airbnb/lottie-ios", from: "3.1.8"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -30,8 +25,7 @@ let package = Package(
         .target(
             name: "UIExtensions",
             dependencies: [.product(name: "CombineRex", package: "SwiftRex"),
-                           "FoundationExtensions",
-                           "Lottie"]),
+                           "FoundationExtensions"]),
         .testTarget(
             name: "UIExtensionsTests",
             dependencies: ["UIExtensions"]),
