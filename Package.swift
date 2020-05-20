930a30c594183ed79c648f84a804754a9d1a1ddc
Luis Reisewitz <luis.reisewitz@teufel.de>
2020-05-20 13:59:19 +0200

    Re-add all Platforms

--- Package.swift
+++ Package.swift
@@ -6,7 +6,10 @@ import PackageDescription
 let package = Package(
     name: "UIExtensions",
     platforms: [
+        .macOS(SupportedPlatform.MacOSVersion.v10_15),
         .iOS(SupportedPlatform.IOSVersion.v13),
+        .tvOS(SupportedPlatform.TVOSVersion.v13),
+        .watchOS(SupportedPlatform.WatchOSVersion.v6)
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
