0a673ef7b4b79c005990deafdb0186eb3d97238f
Luis Reisewitz <luis.reisewitz@teufel.de>
2020-10-01 14:23:42 +0200

    Add View+EdgeBorder

---
+++ Sources/UIExtensions/Extensions/View+EdgeBorder.swift
@@ -0,0 +1,57 @@
+//
+//  View+EdgeBorder.swift
+//  UIExtensions
+//
+//  Created by Luis Reisewitz on 01.10.20.
+//  Copyright © 2020 Lautsprecher Teufel GmbH. All rights reserved.
+//
+
+import SwiftUI
+
+// Following code is from https://stackoverflow.com/a/58632759
+
+extension View {
+    public func border(width: CGFloat, edges: [Edge], color: Color) -> some View {
+        overlay(EdgeBorder(width: width, edges: edges).foregroundColor(color))
+    }
+}
+
+private struct EdgeBorder: Shape {
+    var width: CGFloat
+    var edges: [Edge]
+
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        for edge in edges {
+            var x: CGFloat {
+                switch edge {
+                case .top, .bottom, .leading: return rect.minX
+                case .trailing: return rect.maxX - width
+                }
+            }
+
+            var y: CGFloat {
+                switch edge {
+                case .top, .leading, .trailing: return rect.minY
+                case .bottom: return rect.maxY - width
+                }
+            }
+
+            var w: CGFloat {
+                switch edge {
+                case .top, .bottom: return rect.width
+                case .leading, .trailing: return self.width
+                }
+            }
+
+            var h: CGFloat {
+                switch edge {
+                case .top, .bottom: return self.width
+                case .leading, .trailing: return rect.height
+                }
+            }
+            path.addPath(Path(CGRect(x: x, y: y, width: w, height: h)))
+        }
+        return path
+    }
+}
