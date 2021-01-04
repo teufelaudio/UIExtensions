b8aaaa5dfe2f47ca2f2860b13d3b2450abc1e511
Luis Reisewitz <zweigraf@users.noreply.github.com>
2021-01-04 12:48:22 +0100

    Add View.debug() and Binding.debug() functions (#18)
    Both allow to execute a closure when the view is rendered or the Binding is accessed, respectively.

---
+++ Sources/UIExtensions/Extensions/View+Debug.swift
@@ -0,0 +1,31 @@
+//
+//  View+Debug.swift
+//  UIExtensions
+//
+//  Created by Luis Reisewitz on 11.12.20.
+//  Copyright © 2020 Lautsprecher Teufel GmbH. All rights reserved.
+//
+
+// The following additions are only to be used in Debug mode.
+#if DEBUG && canImport(SwiftUI)
+import SwiftUI
+
+extension View {
+    /// Allows to execute a function whenever this view is rendered. This is a useful helper for debugging
+    /// View state when rendering. Can be used to e.g. print something whenever this view is re-created
+    /// (the body is accessed).
+    ///
+    /// Usage:
+    ///
+    ///     .debug {
+    ///         print("Print something whenever view is rendered")
+    ///     }
+    ///
+    /// - Parameter closure: Closure to execute whenever this view hierarchy is created.
+    /// - Returns: The same view this was called, chainable.
+    public func debug(_ closure: () -> Void) -> Self {
+        closure()
+        return self
+    }
+}
+#endif
