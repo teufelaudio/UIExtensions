f2af280a9e8385778b351c22935ae330c97b1f0a
Giulia Ariu <giulia.ariu@teufel.de>
2021-09-06 16:39:31 +0200

    Added background colour to circled number

--- Sources/UIExtensions/Components/SwiftUI/Pure/CircledNumber.swift
+++ Sources/UIExtensions/Components/SwiftUI/Pure/CircledNumber.swift
@@ -6,23 +6,32 @@ public struct CircledNumber<Content: View, S: ShapeStyle>: View {
     private let length: CGFloat
     private let content: Content
     private let strokeContent: S
+    private let backgroundColor: Color
 
-    public init(number: Int, length: CGFloat, strokeContent: S, @ViewBuilder content: @escaping (Int) -> Content) {
+    public init(number: Int, length: CGFloat, strokeContent: S, backgroundColor: Color = Color.clear, @ViewBuilder content: @escaping (Int) -> Content) {
         self.content = content(number)
         self.length = length
         self.strokeContent = strokeContent
+        self.backgroundColor = backgroundColor
     }
     public var body: some View {
         content
-            .squared(length: length)
-            .background(Circle().stroke(strokeContent), alignment: .center)
+
+            .frame(width: length, height: length, alignment: .center)
+            .background(Circle()
+                            .stroke(strokeContent), alignment: .center)
+            .background(Circle().fill(backgroundColor))
     }
 }
 
 #if DEBUG
 struct CircledNumberPreviews: PreviewProvider {
     static var previews: some View {
-        CircledNumber(number: 9, length: 18, strokeContent: Color.black) { Text("\($0)") }
+        VStack(spacing: 20) {
+            CircledNumber(number: 9, length: 18, strokeContent: Color.black, backgroundColor: Color.red) { Text("\($0)") }
+
+            CircledNumber(number: 9, length: 18, strokeContent: Color.red) { Text("\($0)") }
+        }
     }
 }
 #endif
