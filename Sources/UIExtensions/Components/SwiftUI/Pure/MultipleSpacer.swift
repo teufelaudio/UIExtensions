f8601cb45fd9dac7fc036d896e01c89923251b2b
Giulia Ariu <37103218+GiuliaAriu@users.noreply.github.com>
2021-09-30 12:48:15 +0200

    Allow multiple spacer to have a minLength to pass to the spacer initialiser (#29)

--- Sources/UIExtensions/Components/SwiftUI/Pure/MultipleSpacer.swift
+++ Sources/UIExtensions/Components/SwiftUI/Pure/MultipleSpacer.swift
@@ -10,16 +10,18 @@ import SwiftUI
 
 public struct MultipleSpacer: View {
     private let multiplier: Int
+    private let minLength: CGFloat?
 
-    public init(multiplier: Int) {
+    public init(multiplier: Int, minLength: CGFloat? = nil) {
         self.multiplier = multiplier
+        self.minLength = minLength
     }
 
     public var body: some View {
         Group {
             if multiplier > 0 {
                 ForEach(1...multiplier, id: \.self) { _ in
-                    Spacer()
+                    Spacer.init(minLength: minLength)
                 }
             }
         }
