52027900e2bf53b280e9c568f89e7188831e6bc6
Lukas Liebl <Lukas.Liebl@Teufel.de>
2023-08-25 10:44:29 +0200

    Adds shake gesture view modifier implementing override on motionEnded on UIWindow using NotificationCenter to broadcast motion detection

---
+++ Sources/UIExtensions/Extensions/UIWindow+MotionEnded.swift
@@ -0,0 +1,19 @@
+// Copyright © 2023 Lautsprecher Teufel GmbH. All rights reserved.
+
+import UIKit
+
+extension NSNotification.Name {
+    public static let didShakeDevice = NSNotification.Name("DidShakeDeviceNotification")
+}
+
+extension UIWindow {
+    override open func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        super.motionEnded(motion, with: event)
+        
+        switch event?.subtype {
+        case .motionShake:
+            NotificationCenter.default.post(name: .didShakeDevice, object: event)
+        default: break
+        }
+    }
+}
