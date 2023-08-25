b62dd45cd2d40433fa4f48d5a0bd3f5a02679d57
Lukas Liebl <32835774+LukasLiebl@users.noreply.github.com>
2023-08-25 12:42:45 +0200

    Adds shake gesture view modifier (#64)
    Adds shake gesture view modifier implementing override on motionEnded on UIWindow using NotificationCenter to broadcast motion detection

---
+++ Sources/UIExtensions/ViewModifiers/ShakeGestureViewModifier.swift
@@ -0,0 +1,19 @@
+// Copyright © 2023 Lautsprecher Teufel GmbH. All rights reserved.
+
+import SwiftUI
+
+struct ShakeGestureViewModifier: ViewModifier {
+  let action: () -> Void
+  
+  func body(content: Content) -> some View {
+    content.onReceive(NotificationCenter.default.publisher(for: .didShakeDevice)) { _ in
+        action()
+    }
+  }
+}
+
+extension View {
+  public func onShakeGesture(perform action: @escaping () -> Void) -> some View {
+    self.modifier(ShakeGestureViewModifier(action: action))
+  }
+}
