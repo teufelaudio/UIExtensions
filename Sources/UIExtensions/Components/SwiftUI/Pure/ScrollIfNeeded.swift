660d08d49d9524a7c4a35ebe0f81224feff072f3
Lukas Liebl <Lukas.Liebl@Teufel.de>
2024-10-01 13:41:11 +0200

    Implements a SwiftUI ScrollIfNeeded component for iOS 16.4 and upwards to wrap views that should scroll if the space is limited

---
+++ Sources/UIExtensions/Components/SwiftUI/Pure/ScrollIfNeeded.swift
@@ -0,0 +1,29 @@
+// Copyright © 2024 Lautsprecher Teufel GmbH. All rights reserved.
+
+import SwiftUI
+
+@available(iOS 16.4, *)
+/// A view that embeds content in a scroll view if content overflows.
+public struct ScrollIfNeeded<Content: View>: View {
+    private let content: () -> Content
+    private let axis: Axis.Set
+
+    /// Initializes a new instance of `ScrollIfNeeded`.
+    ///
+    /// - Parameters:
+    ///   - axis: The axis along which scrolling should occur. Defaults to vertical.
+    ///   - content: A closure returning the content to embed within the scroll view.
+    public init(
+        in axis: Axis.Set = [.vertical],
+        @ViewBuilder
+        content: @escaping () -> Content
+    ) {
+        self.axis = axis
+        self.content = content
+    }
+
+    public var body: some View {
+        ScrollView(axis, content: content)
+            .scrollBounceBehavior(.basedOnSize)
+    }
+}
